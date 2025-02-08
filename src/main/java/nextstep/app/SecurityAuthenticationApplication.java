faf6b89b569069d67fac6b2090953f6112014dff
haero77 <shineecard5@gmail.com>
2025-02-08 19:18:25 +0900

    feat(step1): FilterChainProxy를 빈으로 등록하는 WebSecurityConfiguration 구현

--- src/main/java/nextstep/app/SecurityAuthenticationApplication.java
+++ src/main/java/nextstep/app/SecurityAuthenticationApplication.java
@@ -1,12 +1,13 @@
 package nextstep.app;
 
 import nextstep.security.config.SecurityFilterAutoConfiguration;
+import nextstep.security.config.WebSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(SecurityFilterAutoConfiguration.class)
+@Import({SecurityFilterAutoConfiguration.class, WebSecurityConfiguration.class})
 public class SecurityAuthenticationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/nextstep/security/config/WebSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package nextstep.security.config;
+
+import nextstep.security.FilterChainProxy;
+import nextstep.security.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * "springSecurityFilterChain"이라는 이름으로 FilterChainProxy를 빈으로 등록한다.
+ */
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean(name = FilterChainProxy.FILTER_CHAIN_PROXY_BEAN_NAME)
+    public FilterChainProxy filterChainProxy(List<SecurityFilterChain> securityFilterChains) {
+        return new FilterChainProxy(securityFilterChains);
+    }
+}
