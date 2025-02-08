615831f67b1cac50601fa3bdc2b022a65af0550c
haero77 <shineecard5@gmail.com>
2025-02-08 19:06:50 +0900

    feat(step1): DelegatingFilterProxy를 필터로 등록하는 SecurityFilterAutoConfiguration 구현

--- src/main/java/nextstep/app/SecurityAuthenticationApplication.java
+++ src/main/java/nextstep/app/SecurityAuthenticationApplication.java
@@ -1,9 +1,12 @@
 package nextstep.app;
 
+import nextstep.security.config.SecurityFilterAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SecurityFilterAutoConfiguration.class)
 public class SecurityAuthenticationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/nextstep/security/config/SecurityFilterAutoConfiguration.java
@@ -0,0 +1,24 @@
+package nextstep.security.config;
+
+import jakarta.servlet.Filter;
+import nextstep.security.FilterChainProxy;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+/**
+ * DelegatingFilterProxy를 FilterChainProxy를 참조하게 한 후 빈으로 등록한다.
+ */
+@Configuration
+public class SecurityFilterAutoConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<Filter> springSecurityFilterChainRegistration() {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+
+        registration.setFilter(new DelegatingFilterProxy(FilterChainProxy.FILTER_CHAIN_PROXY_BEAN_NAME));
+
+        return registration;
+    }
+}
