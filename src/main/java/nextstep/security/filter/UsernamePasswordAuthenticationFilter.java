4244c3374492c3dfa7117895a1d2b7f232e04173
haero77 <shineecard5@gmail.com>
2025-02-12 22:05:10 +0900

    refactor(feedback): 인증 필터가 요청당 한 번만 수행되는 것을 보장하도록 OncePerRequestFilter를 상속하도록 변경
    - applies feedback https://github.com/next-step/spring-security-authentication/pull/40#discussion_r1948038644

--- src/main/java/nextstep/security/filter/UsernamePasswordAuthenticationFilter.java
+++ src/main/java/nextstep/security/filter/UsernamePasswordAuthenticationFilter.java
@@ -1,19 +1,20 @@
 package nextstep.security.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import nextstep.security.AuthenticationException;
 import nextstep.security.authentication.*;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 // 인증 정보를 추출하고, AuthenticationManager 에게 비밀번호 맞고 틀리는 것을 검증하는 책임을 위임한다.
-public class UsernamePasswordAuthenticationFilter implements Filter {
+public class UsernamePasswordAuthenticationFilter extends OncePerRequestFilter {
 
     private static final String SPRING_SECURITY_CONTEXT_KEY = "SPRING_SECURITY_CONTEXT";
 
@@ -28,26 +29,23 @@ public class UsernamePasswordAuthenticationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(
-            ServletRequest servletRequest,
-            ServletResponse servletResponse,
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return !AUTHENTICATION_NEED_PATHS.contains(request.getRequestURI());
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
             FilterChain filterChain
-    ) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-
-        if (!AUTHENTICATION_NEED_PATHS.contains(httpRequest.getRequestURI())) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
+    ) throws IOException {
         // extract username and password
-        Map<String, String[]> parameterMap = httpRequest.getParameterMap();
+        Map<String, String[]> parameterMap = request.getParameterMap();
         String username = parameterMap.get("username")[0];
         String password = parameterMap.get("password")[0];
 
         if (!StringUtils.hasText(username) || !StringUtils.hasText(password)) {
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "username or password is empty");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "username or password is empty");
             return;
         }
 
@@ -55,10 +53,10 @@ public class UsernamePasswordAuthenticationFilter implements Filter {
 
         try {
             Authentication authenticated = this.authenticationManager.authenticate(authRequest);
-            HttpSession session = httpRequest.getSession();
+            HttpSession session = request.getSession();
             session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, authenticated);
         } catch (AuthenticationException e) {
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "username or password is empty");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "username or password is empty");
         }
     }
 }
