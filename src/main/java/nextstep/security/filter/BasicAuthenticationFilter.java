6018198c8b5ad963f844fd570e4448cdc40f9b21
haero77 <shineecard5@gmail.com>
2025-02-12 22:05:10 +0900

    refactor(feedback): 인증 필터가 요청당 한 번만 수행되는 것을 보장하도록 OncePerRequestFilter를 상속하도록 변경
    - applies feedback https://github.com/next-step/spring-security-authentication/pull/40#discussion_r1948038644

--- src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
+++ src/main/java/nextstep/security/filter/BasicAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package nextstep.security.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.security.AuthenticationException;
@@ -8,11 +9,12 @@ import nextstep.security.authentication.*;
 import nextstep.security.util.Base64Convertor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
-public class BasicAuthenticationFilter implements Filter {
+public class BasicAuthenticationFilter extends OncePerRequestFilter {
 
     private static final Logger log = LoggerFactory.getLogger(BasicAuthenticationFilter.class);
 
@@ -27,37 +29,39 @@ public class BasicAuthenticationFilter implements Filter {
     }
 
     @Override
-    public void doFilter(
-            ServletRequest servletRequest,
-            ServletResponse servletResponse,
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        // '/members' 에 대한 요청이 아니면 필터링 하지 말라는 의미. 즉, '/members'에 대한 요청만 필터링
+        return !AUTHENTICATION_NEED_PATHS.contains(request.getRequestURI());
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
             FilterChain filterChain
-    ) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
-
-        if (!AUTHENTICATION_NEED_PATHS.contains(httpRequest.getRequestURI())) {
-            filterChain.doFilter(servletRequest, servletResponse);
-            return;
-        }
-
-        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
-
+    ) throws ServletException, IOException {
         try {
-            String authorization = httpRequest.getHeader("Authorization");
-            String credentials = authorization.split(" ")[1]; // "Basic " 뒤의 문자열
-            String decodedString = Base64Convertor.decode(credentials);
-            String[] usernameAndPassword = decodedString.split(":");
-            String username = usernameAndPassword[0];
-            String password = usernameAndPassword[1];
+            Authentication authenticated = attemptAuthentication(request);
+            request.setAttribute("userDetails", authenticated); // 이후 필터에서 사용 가능하도록 인증된 사용자 정보를 저장
 
-            Authentication authRequest = UsernamePasswordAuthenticationToken.unAuthenticated(username, password);
-            Authentication authenticated = this.authenticationManager.authenticate(authRequest);
-
-            httpRequest.setAttribute("userDetails", authenticated); // 이후 필터에서 사용 가능하도록 인증된 사용자 정보를 저장
-
-            filterChain.doFilter(servletRequest, servletResponse);
+            filterChain.doFilter(request, response);
         } catch (AuthenticationException | RuntimeException e) {
             log.debug("Authentication failed", e);
-            httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed");
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed");
         }
+    }
+
+    private Authentication attemptAuthentication(HttpServletRequest request) throws AuthenticationException {
+        String authorization = request.getHeader("Authorization");
+        String credentials = authorization.split(" ")[1];
+        String decodedString = Base64Convertor.decode(credentials);
+        String[] usernameAndPassword = decodedString.split(":");
+
+        Authentication authRequest = UsernamePasswordAuthenticationToken.unAuthenticated(
+                usernameAndPassword[0],
+                usernameAndPassword[1]
+        );
+
+        return this.authenticationManager.authenticate(authRequest);
     }
 }
